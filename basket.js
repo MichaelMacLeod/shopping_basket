d620eefb7c0fb22e32e55c86303ebcbbdbd0cf6a
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-01-26 15:37:55 +0000

    Added inital files

---
+++ basket.js
@@ -0,0 +1,32 @@
+var bread = {
+  price: 150, 
+  bogof: false
+}
+
+var jam = {
+  price: 90,
+  bogof: true,
+}
+
+var beer = {
+  price: 200, 
+  bogof: false
+}
+
+var crisps = {
+  price: 70, 
+  bogof: true
+}
+
+var apples = {
+  price 60,
+  bogof: true
+}
+
+module.exports = bread;
+module.exports = jam;
+module.exports = beer;
+module.exports = crisps;
+module.exports = apples;
+
+
---
+++ shopping_checker.js
@@ -0,0 +1,24 @@
+var shopper = require('./basket');
+var assert = require('assert');
+
+describe("The shopper's ", function(){
+//1. basket should be empty at start: 
+
+});
+
+//2. should be able to add many items to basket: 
+
+//3. should get 10% discount off a basket worth over £20: 
+
+//We need to find the price of a shopping basket.
+
+// Discounts:
+
+// 10% discount for all shopping baskets over £20
+
+// Certain items are buy one get on free, this should be considered.
+
+// If the customer has a valid discount card then they get and additional 5% off.
+
+// Test drive your objects.
+// Commit after every passing test. 
