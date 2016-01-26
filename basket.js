523fdba19aa42792ec930bb2a4c5a115a8f9e83c
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-01-26 17:25:26 +0000

    set discount checker

--- basket.js
+++ basket.js
@@ -38,6 +38,8 @@ var myBasket = {
       this.cost = this.cost + price;
     }
   },
+
+  
 }
 
 
--- shopping_checker.js
+++ shopping_checker.js
@@ -5,22 +5,25 @@ var assert = require('assert');
 
 describe("Shopping Basket", function(){
   it('should tell us the total is ', function(){
-  shopper.totalCost();
-  assert.equal(570, shopper.cost);
+    shopper.totalCost();
+    assert.equal(570, shopper.cost);
+  });
+
+
+  //2. should get 10% discount off a basket worth over £20: 
+  it('should reduce the total cost by 10% if the basket is worth over £20', function(){
+    shopper.discount();
+    assert.equal(564.3, )
   });
 
 
 
-});
 
 
-//move total cost function to basket. 
 
-//3. should get 10% discount off a basket worth over £20: 
+//3. Certain items are buy one get one free, this should be considered. 
 
-//4. Certain items are buy one get on free, this should be considered. 
-
-//5. If the customer has a valid discount card then they get and additional 5% off. 
+//4. If the customer has a valid discount card then they get and additional 5% off. 
 
 
 // Test drive your objects.
